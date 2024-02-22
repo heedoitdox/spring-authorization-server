fe30d1f8c848d207c20d95b060e826ec8aa596d8
lewisdhseo91 <lewis@barogo.com>
2024-02-23 01:55:01 +0900

    feat(authorization-server): 인증 서버 토큰 생성 jwt customizer wip

--- authorization-server/src/main/java/com/server/authorization/config/AuthorizationServerConfig.java
+++ authorization-server/src/main/java/com/server/authorization/config/AuthorizationServerConfig.java
@@ -10,6 +10,9 @@ import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.UUID;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,16 +22,30 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.OAuth2Token;
 import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.token.DelegatingOAuth2TokenGenerator;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.JwtGenerator;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenGenerator;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.util.StringUtils;
 
 @Configuration
 public class AuthorizationServerConfig {
@@ -37,6 +54,8 @@ public class AuthorizationServerConfig {
   @Order(Ordered.HIGHEST_PRECEDENCE)
   public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
     OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+    http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+        .tokenGenerator(jwtTokenGenerator());
 
 //    http
 //        // Accept access tokens for User Info and/or Client Registration
@@ -44,6 +63,45 @@ public class AuthorizationServerConfig {
 //            .jwt(Customizer.withDefaults()));
 
     return http.build();
+  }
+
+  @Bean
+  public OAuth2TokenGenerator<OAuth2Token> jwtTokenGenerator() {
+    JwtEncoder jwtEncoder = new NimbusJwtEncoder(jwkSource());
+    JwtGenerator jwtGenerator = new JwtGenerator(jwtEncoder);
+    jwtGenerator.setJwtCustomizer(jwtTokenCustomizer());
+    return new DelegatingOAuth2TokenGenerator(jwtGenerator);
+  }
+
+  @Bean
+  public OAuth2TokenCustomizer<JwtEncodingContext> jwtTokenCustomizer() {
+    return (context) -> {
+      if (OAuth2TokenType.ACCESS_TOKEN.equals(context.getTokenType())) {
+        String issuer;
+        if (context.getAuthorizationServerContext() != null) {
+          issuer = context.getAuthorizationServerContext().getIssuer();
+        } else {
+          issuer = null;
+        }
+
+        JwtClaimsSet.Builder claimsBuilder = context.getClaims();
+        claimsBuilder.claims(claims -> {
+
+        });
+        if (StringUtils.hasText(issuer)) {
+          claimsBuilder.issuer(issuer);
+        }
+        claimsBuilder
+            .subject(context.getPrincipal().getName())
+            .audience(Collections.unmodifiableList(Arrays.asList("", "")))
+            .issuedAt(Instant.now())
+            .expiresAt(Instant.now().plus(context.getRegisteredClient()
+                .getTokenSettings().getAccessTokenTimeToLive()))
+            .id(UUID.randomUUID().toString());
+
+        claimsBuilder.build();
+      }
+    };
   }
 
   @Bean
@@ -86,8 +144,7 @@ public class AuthorizationServerConfig {
       KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
       keyPairGenerator.initialize(2048);
       keyPair = keyPairGenerator.generateKeyPair();
-    }
-    catch (Exception ex) {
+    } catch (Exception ex) {
       throw new IllegalStateException(ex);
     }
     return keyPair;
@@ -96,6 +153,11 @@ public class AuthorizationServerConfig {
   @Bean
   public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
     return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+  }
+
+  @Bean
+  public JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwkSource) {
+    return new NimbusJwtEncoder(jwkSource);
   }
 
   @Bean
