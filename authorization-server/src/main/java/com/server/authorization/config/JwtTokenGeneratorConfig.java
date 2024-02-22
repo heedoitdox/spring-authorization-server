b67a5127aaa834b5576f01249c1a4909b4ab7ce5
lewisdhseo91 <lewis@barogo.com>
2024-02-23 02:57:50 +0900

    feat(authorization-server): 인증 서버 토큰 생성 rsa 키 세팅 추가

--- authorization-server/src/main/java/com/server/authorization/config/JwtTokenGeneratorConfig.java
+++ authorization-server/src/main/java/com/server/authorization/config/JwtTokenGeneratorConfig.java
@@ -5,11 +5,17 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.UUID;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
@@ -42,7 +48,7 @@ public class JwtTokenGeneratorConfig {
   }
 
   @Bean
-  public OAuth2TokenGenerator<OAuth2Token> jwtTokenGenerator() {
+  public OAuth2TokenGenerator<OAuth2Token> jwtTokenGenerator() throws Exception {
     JwtEncoder jwtEncoder = new NimbusJwtEncoder(jwkSource());
     JwtGenerator jwtGenerator = new JwtGenerator(jwtEncoder);
     jwtGenerator.setJwtCustomizer(jwtTokenCustomizer(users()));
@@ -50,16 +56,31 @@ public class JwtTokenGeneratorConfig {
   }
 
   @Bean
-  public JWKSource<SecurityContext> jwkSource() {
-    KeyPair keyPair = generateRsaKey();
-    RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-    RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+  public JWKSource<SecurityContext> jwkSource() throws Exception {
+    String publicKeyStr = "";
+    String privateKeyStr = "";
+    RSAPublicKey publicKey = (RSAPublicKey) getPublicKeyFromString(publicKeyStr);
+    RSAPrivateKey privateKey = (RSAPrivateKey) getPrivateKeyFromString(privateKeyStr);
     RSAKey rsaKey = new RSAKey.Builder(publicKey)
         .privateKey(privateKey)
         .keyID(UUID.randomUUID().toString())
         .build();
     JWKSet jwkSet = new JWKSet(rsaKey);
     return new ImmutableJWKSet<>(jwkSet);
+  }
+
+  private PublicKey getPublicKeyFromString(String publicKeyStr) throws Exception {
+    byte[] publicBytes = Base64.decodeBase64(publicKeyStr);
+    X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicBytes);
+    KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+    return keyFactory.generatePublic(keySpec);
+  }
+
+  private PrivateKey getPrivateKeyFromString(String privateKeyStr) throws Exception {
+    byte[] privateBytes = Base64.decodeBase64(privateKeyStr);
+    PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privateBytes);
+    KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+    return keyFactory.generatePrivate(keySpec);
   }
 
   private static KeyPair generateRsaKey() {
